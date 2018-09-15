33b1bfe370a4b8c3a4e01170ff080810bb47ccae
HackerLifeX <43289306+HackerLifeX@users.noreply.github.com>
2018-09-15 09:30:01 +0300

    Auto use helper

---
+++ AutoUse.lua
@@ -0,0 +1,28 @@
+local AutoUse = {}
+
+AutoUse.optionStick = Menu.AddOptionBool({"Hero Specific", "AutoUse"}, "Stick", false)
+AutoUse.optionBottle = Menu.AddOptionBool({"Hero Specific", "AutoUse"}, "Bottle", false)
+
+function AutoUse.OnUpdate()
+	if not Menu.IsEnabled(AutoUse.optionEnableStick) then return end
+	
+	local myHero = Heroes.GetLocal()
+	local stick1 = NPC.GetItem(myHero, "item_magic_stick")
+
+	if not stick1 or not Entity.IsAlive(myHero) then return end
+
+	if Ability.IsReady(stick1) and Entity.GetHealth(myHero) < 75 then 
+		Ability.CastNoTarget(stick1) 
+    end
+	
+	if not Menu.IsEnabled(AutoUse.optionBottle) then return end
+	
+	local bottle = NPC.GetItem(myHero, "item_bottle")
+   
+	if NPC.HasModifier(myHero, "modifier_fountain_aura_buff") and Ability.IsReady(bottle) and
+	Entity.GetHealth(myHero) < Entity.GetMaxHealth(myHero) or NPC.GetMana(myHero) < NPC.GetMaxMana(myHero) then
+	    Ability.CastNoTarget(bottle) 
+	end
+end
+	
+return AutoUse
